4becf8fe3e3e8c834cae7df60d839c6b9dfea793
Simo Khouili <m.simokhouili@gmail.com>
2020-02-23 14:32:47 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -4,7 +4,7 @@ def first_challenge
         :patriarch => {name: "Lord Montague", age: "53"},
         :matriarch => {name: " Lady Montague", age: "54"},
         :heroine   => {name: "Romeo", age: "15", status: "alive"},
-        :heroine_friends  => []  
+        :heroine_friends  => []
      },
      :capulet =? {
        :patriarch => {name: "Lord Capulet", age: "50"},
