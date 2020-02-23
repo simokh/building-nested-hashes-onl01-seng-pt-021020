b005a223742c0834f3dfeef729859ed479f6ffc6
Simo Khouili <m.simokhouili@gmail.com>
2020-02-23 14:47:24 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,20 +10,17 @@ def third_challenge
       :hero => {
         name: "Romeo", age: "15", status: "alive"
         },
-      :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}
-      ]
+      :hero_friends => []
    },
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Capulet", age: "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Capulet", age: "51"
         },
       :heroine => {
-        #your key/value pairs here
+        name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => []
    }
