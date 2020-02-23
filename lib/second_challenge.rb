e4853675aa60627fae7f33db71826c0502ceed4c
Simo Khouili <m.simokhouili@gmail.com>
2020-02-23 14:48:58 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -12,25 +12,6 @@ def second_challenge
       :heroine   => {},
       :heroine_friends  => []
       }
-
+}
 
 end
-
-:montague => {
-   :patriarch => {name: "Lord Montague", age: "53"},
-   :matriarch => {name: " Lady Montague", age: "54"},
-   :heroine   => {name: "Romeo", age: "15", status: "alive"},
-   :heroine_friends  => [
-     {name: "Benvolio", age: "17", attitude: "worried"},
-     {name: "Mercutio", age: "18", attitude: "hot-headed"}
-   ]
-},
-:capulet =? {
-  :patriarch => {name: "Lord Capulet", age: "50"},
-  :matriarch => {name: "Lady Capulet", age: "51"},
-  :heroine   => {name: "Juliet", age: "15", status: "alive"},
-  :heroine_friends  => [
-    {name: "Steven", age: "30", attitude: "confused"},
-     {name: "Nurse", age: "44", attitude: "worried"}
-  ]
-}
